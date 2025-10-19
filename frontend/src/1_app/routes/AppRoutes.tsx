217079a1786a22421f8ebad6ae1fa52cc93c8b00
Kudaiberdi <kdajberdiz@gmail.com>
2025-10-19 20:56:01 +0500

    start project

---
+++ frontend/src/1_app/routes/AppRoutes.tsx
@@ -0,0 +1,12 @@
+const AppRoutes = () => {
+  return (
+    <>
+      <p className="border rounded max-w-xl mx-auto text-center mt-[200px] text-3xl">
+        {" "}
+        AILab project tracker!{" "}
+      </p>
+    </>
+  );
+};
+
+export default AppRoutes;
